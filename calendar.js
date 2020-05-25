da285d3f98cd5a6c3a22bded64902c611e7901f8
alelevy15 <alelevy15@gmail.com>
2020-05-24 21:37:37 -0300

    load festives days from API and paint them on the calendar

--- calendar.js
+++ calendar.js
@@ -6,68 +6,82 @@ const today = new Date();
 
 var displayedMonth = new Date().getMonth();
 var displayedYear = new Date().getFullYear();
+var displayedDates = getDaysOfMonth(displayedMonth, displayedYear);
 
 window.onload = function () {
   initUI()
 };
 
 function initUI() {
+  initControls();
+  loadDaysToCalendar();
+  paintFestivesDays();
+}
+
+function initControls(){
   document.getElementById('month').textContent = monthNames[today.getMonth()];
   document.getElementById('year').textContent = today.getFullYear();
   document.getElementById('next').addEventListener('click', loadNextMonth);
   document.getElementById('previous').addEventListener('click', loadPreviousMonth);
+}
 
-  loadDaysToCalendar(today.getMonth(), today.getFullYear());
+function loadDaysToCalendar() {
+  clearGrid();
+  var indexToInsert = displayedDates[0].weekday;
+  displayedDates.forEach(date => {
+    document.getElementById(`day-${indexToInsert}`).textContent = date.day;
+    indexToInsert++;
+  });
 }
 
 function getDaysOfMonth(month, year) {
   var date = new Date(year, month, 1);
-  var getDaysOfMonth = [];
+  var daysOfMonth = [];
   while (date.getMonth() == month) {
-    getDaysOfMonth.push(new Date(date));
+    daysOfMonth.push({
+      day: new Date(date).getDate(),
+      month: new Date(date).getMonth(),
+      year: new Date(date).getFullYear(),
+      weekday: new Date(date).getDay(),
+    })
     date.setDate(date.getDate() + 1)
   }
-  return getDaysOfMonth;
+  return daysOfMonth;
 }
 
-function loadDaysToCalendar(month, year) {
-  clearGrid();
-  var daysOfMonth = getDaysOfMonth(month, year);
-  var indexToInsert = daysOfMonth[0].getDay();
-  daysOfMonth.forEach(day => {
-    document.getElementById(`day-${indexToInsert}`).textContent = day.getDate();
-    indexToInsert++;
-  });
-  loadFestiveDays();
-  paintCurrentDay();
-}
+  // if (displayedMonth == today.getMonth() && displayedYear == today.getFullYear())
+  //   paintCurrentDay();
+
 
 function clearGrid() {
   for (let i = 0; i < 42; i++) {
     document.getElementById(`day-${i}`).textContent = '';
+    document.getElementById(`day-${i}`).classList.remove('festive');
   }
 }
 
 function loadNextMonth() {
-  if (displayedMonth == 11) {
-    displayedMonth = 0;
-    displayedYear++;
-  } else {
+  displayedMonth == 11 ?
+    (displayedMonth = 0,
+      displayedYear++) :
     displayedMonth++;
-  }
+  
+  displayedDates = getDaysOfMonth(displayedMonth, displayedYear);
   updateDisplayedValues();
-  loadDaysToCalendar(displayedMonth, displayedYear);
+  loadDaysToCalendar();
+  paintFestivesDays();
 }
 
 function loadPreviousMonth() {
-  if (displayedMonth == 0) {
-    displayedMonth = 11;
-    displayedYear--;
-  } else {
+  displayedMonth == 0 ?
+    (displayedMonth = 11,
+      displayedYear--) :
     displayedMonth--;
-  }
+
+  displayedDates = getDaysOfMonth(displayedMonth, displayedYear);
   updateDisplayedValues();
-  loadDaysToCalendar(displayedMonth, displayedYear);
+  loadDaysToCalendar();
+  paintFestivesDays();
 }
 
 function updateDisplayedValues() {
@@ -75,10 +89,23 @@ function updateDisplayedValues() {
   document.getElementById('year').textContent = displayedYear;
 }
 
-function loadFestiveDays() {
-
+function paintFestivesDays() {
+  getFestivesDayOfTheYear(displayedYear).then(festives =>{
+   Object.entries(festives[displayedMonth]).forEach(festiveDay =>{
+     let indexOfFestiveDay = displayedDates[0].weekday + parseInt(festiveDay[0]) - 1;
+     document.getElementById(`day-${indexOfFestiveDay}`).classList.add('festive');
+   });
+  })
 }
+
+function getFestivesDayOfTheYear(year) {
+  let url = `http://nolaborables.com.ar/api/v2/feriados/${year}?formato=mensual`;
+  return fetch(url).then(res => res.json()).then(json => {return json});
+}
+
 
 function paintCurrentDay() {
 
 }
+
+document.getElementById("MyElement").classList.add('MyClass');
